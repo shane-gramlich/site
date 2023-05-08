e921746bfe7ab5d23975773bfc1cc39abae1d2a9
Shane Gramlich <shane@gramlich.co>
2023-05-08 01:24:28 -0600

    Add ionic production files

---
+++ dev/ionic/src/views/TestPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>Header Title</ion-title>
+            </ion-toolbar>
+        </ion-header>
+
+        <ion-content>
+            Some Content Here
+
+            <ion-footer>
+                <ion-toolbar>
+                    <ion-title>Footer</ion-title>
+                </ion-toolbar>
+                <ion-item>
+                    <ion-label>Some Label:</ion-label>
+                    <ion-input placeholder="Type something here">Some Input</ion-input>
+                </ion-item>
+            </ion-footer>
+
+        </ion-content>
+
+    </ion-page>
+</template>
